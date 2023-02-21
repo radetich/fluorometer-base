76c4cea55566128df4e24ea78c238b36d6151553
Cole Radetich <3dradd@gmail.com>
2023-02-21 15:51:36 -0700

    Update current.ino
    remove reassign PWM (this was messing with the data from the breadboard test)

--- teensy_4/current/current.ino
+++ teensy_4/current/current.ino
@@ -1,10 +1,10 @@
 #define HWSERIAL Serial1 // this makes it easier for us to reference the TX/RX, since serial as a keyword here references USB OTG mode and not RX/Tx
 const int sensorPin = 14; //sensor0 (hamamatsu)
 const int sensor1Pin = 15; //sensor1 (hammamatsu, probably emission return. Check this.)
-const int ledPin = 2; //driving LEDs
+const int ledPin = 2; //driving LEDs, PWM**
 const int boardLedPin = 13; //little orange fella
 
-int emmReturn, filReturn;
+int emmReturn, filReturn, pwm_on;
 
 //extra pin info: https://www.pjrc.com/store/teensy40.html#pins
 
@@ -15,66 +15,53 @@ void setup()
   HWSERIAL.begin(115200);
   HWSERIAL.setTimeout(100);
   //init debug LED (maybe turn this off for production? or leave it. Could be useful)
-  pinMode(boardLedPin, OUTPUT);
+  //pinMode(boardLedPin, OUTPUT);
 }
 
 void loop() 
 {
-  //make sure we are at 0 for LED
-  analogWrite(boardLedPin, 0);
+  //make sure we are at 0 for LED, set PWM flag value to 0
+  //analogWrite(boardLedPin, 0);
   analogWrite(ledPin, 0);
-  //wait for initial ack
+  pwm_on = 0;
+  //wait for initial ack. This will allow some nonsense data to come through
   if (HWSERIAL.available() > 0) 
   {
     String data = HWSERIAL.readStringUntil('\n');
-    //debug prints. use liberally
+    //return command to pi. this is for debugging.
     //HWSERIAL.print("Pi sent to firmware: ");
     //HWSERIAL.println(data);
 
-    //send ack back w some extra
+    //send ack back w the GOT header, handshake complete!
     HWSERIAL.print("GOT ");
     HWSERIAL.println(data);
     while(data == "BEGIN TRANSFER")
     {
-      analogWrite(boardLedPin, 255);
+      if(pwm_on == 0)
+      {
+        //turn led on and set flag variable. This allows us to begin at 100% of our 4 KHz PWM and remain at that through capture.
+        analogWrite(ledPin, 255);
+        pwm_on = 1;
+      }
+      //orange debug light
+      //analogWrite(boardLedPin, 255);
       //We are transferring! begin transfer.
-      //for now, this uses our breaboard LEDs. Might be the same for actual stuff
-      //probably wont be
 
-      //turn led on
-      analogWrite(ledPin, 255);
-
-      //record values from sensor
+      //record values from sensor until HWSERIAL responds to stop...
       emmReturn = analogRead(sensorPin);
       HWSERIAL.print("EMM ");
       HWSERIAL.println(emmReturn);
       filReturn = analogRead(sensor1Pin); 
       HWSERIAL.print("FIL ");
       HWSERIAL.println(filReturn);
-
-      //wait a lil so we can see the action
-      delay(100);
-      //turn the led off
-      analogWrite(ledPin, 0);
-      //wait a lil so we can see the action
-      delay(100);
       if (HWSERIAL.available() > 0)
       {
         data = HWSERIAL.readStringUntil('\n');
-        analogWrite(boardLedPin, 0);
       }
     }
+    //redundent sometimes but keeps LED off if we are stuck in a recieving loop on the pi side. We don't want to drain the battery in this scenario. The Pi should get itself back on track.
+    //analogWrite(boardLedPin, 0);
+    analogWrite(ledPin, 0);
+    pwm_on = 0;
   }
 }
-
-
-/* old led code
-  analogWrite(ledPin, 255);
-  lightLevel = analogRead(sensorPin);
-  Serial.println(lightLevel);
-  delay(100);
-  analogWrite(ledPin, 0);
-  lightLevel = analogRead(sensorPin); 
-  Serial.println(lightLevel);
-  delay(100);
-  */
